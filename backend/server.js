2d7a1de112ed421732cbda23937fde305ce5a167
sehrishsiddique853 <sehrishsiddique853@gmail.com>
2026-06-11 18:43:02 +0500

    Prepare backend CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,8 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5174",
-];
+  process.env.FRONTEND_URL,
+].filter(Boolean);
 
 app.use(cors({
   origin: allowedOrigins,
